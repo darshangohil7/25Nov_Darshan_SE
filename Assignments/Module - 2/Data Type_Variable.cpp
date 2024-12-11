f19570c16252550a62a880327d1ad71d014cee38
Darshan Gohil <gohildarshan151@gmail.com>
2024-12-11 22:25:59 +0530

    Add files via upload

---
+++ Assignments/Module - 2/Data Type_Variable.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int age=18;
+	float pi=3.14;
+	char str[10]="Darshan";
+	printf("Your Name is:%s",str);
+	printf("\nYour Age is:%d",age);
+	printf("\nThe Value of PI:%.2f",pi);
+}
