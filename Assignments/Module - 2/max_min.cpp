7d54b4c0cd93ef8f4d76339b5a9ff377b703d399
Darshan Gohil <gohildarshan151@gmail.com>
2024-12-12 22:17:08 +0530

    Add files via upload

---
+++ Assignments/Module - 2/max_min.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	int a, b;
+		printf("Enter The Value of A:");
+		scanf("%d",&a);
+		printf("Enter The Value of B:");
+		scanf("%d",&b);
+	if(a>b)
+	{
+		printf("The Value of A is Maximum.");
+	}
+	else
+	{
+		printf("The Value of A is Minimum");
+	}
+}
